d6b03a32ee8acf6f75adda8b2b0d03c0eed7ffb9
RohanSatpathy <88677815+RohanSatpathy@users.noreply.github.com>
2021-08-09 12:51:24 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+
+function setup() {
+createCanvas(400,400);
+jeff = createSprite(30, 30, 50, 50);
+jeff.shapeColor = "pink";
+}
+
+function draw() 
+{
+  background(30);
+ 
+  drawSprites();
+}
+
+
+
+
